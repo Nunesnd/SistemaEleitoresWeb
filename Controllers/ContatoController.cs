2ce41856ca6a9e164d495548be837748a3f67785
Diego Nunes <nunesdiegond@gmail.com>
2023-02-10 11:21:26 -0300

    primeiro commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BancoEleitoresWeb.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Detalhes()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirma()
+        {
+            return View();
+        }
+    }
+}
